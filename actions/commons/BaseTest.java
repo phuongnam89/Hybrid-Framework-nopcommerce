c6693268e2827cb68bc24e61517d58288bab60e0
Nam Phuong <namphuong0205@gmail.com>
2022-12-13 18:14:59 +0700

    Browser driver manager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -7,19 +7,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 private WebDriver driver;
 private String projectPath = System.getProperty("user.dir");
 
 protected WebDriver getBrowserDriver(String browserName) {
 	if(browserName.equals("firefox")) {
-		System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
+		WebDriverManager.firefoxdriver().setup();
 		driver = new FirefoxDriver();
 	} else if(browserName.equals("chrome")) {
-		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	} else if (browserName.equals("edge")) {
-		System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver.exe");
+		//System.setProperty("webdriver.edge.driver", projectPath + "/browserDrivers/msedgedriver.exe");
+		WebDriverManager.edgedriver().setup();
 		driver = new EdgeDriver();
 	} else {
 		throw new RuntimeException("browser name invalid");
